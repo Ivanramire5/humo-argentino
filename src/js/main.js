0902fdec100f91a06ce6b13c2b334a49b5c5d9ac
Ivanramire5 <ivan.r4amire5@gmail.com>
2023-07-14 23:06:20 -0300

    blog de amigos

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+Swal.fire({
+    title: '<b class="titulo-sweet">Bienvenido a Humo Argentino',
+    icon: 'info',
+    confirmButtonText: 'Ingresar',
+    footer: '<span class="alerta">No compartir con nadie esta informacion!!!',
+    width: '50%',
+    background: '#4F709C'
+    }
+)
